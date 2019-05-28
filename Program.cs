677a846bc072d18cf1abee6b3cde5c27d23130c5
sealanguage <elaine.froehlich@gmail.com>
2019-05-27 21:54:40 -0400

    Initial check-in of module emoveKFromList

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace emoveKFromList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definition for singly-linked list:
+            // class ListNode<T> {
+            //   public T value { get; set; }
+            //   public ListNode<T> next { get; set; }
+            // }
+            //
+            ListNode<int> removeKFromList(ListNode<int> l, int k)
+            {
+                if (l == null)
+                {
+                    return l;
+                }
+                Console.WriteLine(l.value);
+
+                while (l.value == k)
+                {
+                    if (l.next == null)
+                    {
+                        return l.next;
+                    }
+                    l = l.next;
+                }
+
+                ListNode<int> trav = l;
+                while (trav.next != null)
+                {
+                    if (trav.next.value == k)
+                    {
+                        trav.next = trav.next.next;
+                        continue;
+                    }
+                    trav = trav.next;
+                }
+                return l;
+            }
+
+        }
+    }
+}
